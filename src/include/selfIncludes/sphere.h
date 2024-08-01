01ba0f5fe58060697e4c7d788e7936418805abef
MaxG <maxpgrabov@gmail.com>
2024-08-01 11:26:07 -0400

    Fixed issues

---
+++ src/include/selfIncludes/sphere.h
@@ -0,0 +1,90 @@
+#define _USE_MATH_DEFINES
+#include <SDL2/SDL.h>
+#include <SelfIncludes/objects.h>
+#include <math.h>
+#include <list>
+
+//Too lazy to rename to circle
+class Sphere : public Object {
+public:
+    int radius;
+    double mass;
+    bool magnetic;
+    Sphere(){
+        this->magnetic = false;
+        this->radius = 1;
+        this->mass = 0; //probably a bad idea
+    }
+    Sphere(double radius, double mass){
+        this->radius = radius;
+        this->mass = mass;
+    }
+
+    void Draw(SDL_Renderer *renderer, int offsetX, int offsetY){
+        int sX = radius;
+        int sY = 0;
+        int tmp = 1 - radius;
+
+        std::list<SDL_Point> points;
+
+        SDL_Point p1 = {this->position.x + offsetX + radius, -this->position.y + offsetY};
+        SDL_Point p2 = {this->position.x + offsetX, -this->position.y + offsetY + radius};
+        SDL_Point p3 = {this->position.x + offsetX, -this->position.y + offsetY - radius};
+        SDL_Point p4 = {this->position.x + offsetX - radius, -this->position.y + offsetY};
+
+        points.push_front(p1);
+        points.push_front(p2);
+        points.push_front(p3);
+        points.push_front(p4);
+
+        while(sX > sY){
+            sY++;
+
+            if(tmp <= 0){
+                tmp += 2*sY + 1;
+            }
+            else {
+                sX--;
+                tmp += 2*sY - 2*sX + 1;
+            }
+
+            if(sX < sY){
+                break;
+            }
+
+            p1 = {sX + this->position.x + offsetX, sY + -this->position.y + offsetY};
+            p2 = {-sX + this->position.x + offsetX, sY + -this->position.y + offsetY};
+            p3 = {sX + this->position.x + offsetX, -sY + -this->position.y + offsetY};
+            p4 = {-sX + this->position.x + offsetX, -sY + -this->position.y + offsetY};
+
+            points.push_front(p1);
+            points.push_front(p2);
+            points.push_front(p3);
+            points.push_front(p4);
+
+            p1 = {sY + this->position.x + offsetX, sX + -this->position.y + offsetY};
+            p2 = {-sY + this->position.x + offsetX, sX + -this->position.y + offsetY};
+            p3 = {sY + this->position.x + offsetX, -sX + -this->position.y + offsetY};
+            p4 = {-sY + this->position.x + offsetX, -sX + -this->position.y + offsetY};
+
+            points.push_front(p1);
+            points.push_front(p2);
+            points.push_front(p3);
+            points.push_front(p4);
+        }
+
+        int l = points.size();
+        int k = 0;
+
+        SDL_Point *p = (SDL_Point*)malloc(sizeof(SDL_Point) * l);
+
+        for(auto const& i : points){
+            p[k].x = i.x;
+            p[k++].y = i.y;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawPoints(renderer, p, l + 1);
+        free(p);
+    }
+};
---
+++ src/include/selfIncludes/textRenderer.h
@@ -0,0 +1,54 @@
+//TODO
+#include <SDL2/SDL.h>
+#include <string>
+
+//8 , 49 start, 11 interval, y end 64
+class TextRenderer {
+public:
+
+    SDL_Texture *texture;
+    SDL_Rect source;
+    SDL_Rect dest;
+
+    TextRenderer(SDL_Renderer *renderer){
+        SDL_Surface *surface = SDL_LoadBMP("./src/include/SelfIncludes/font.bmp");
+        this->texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+        SDL_FreeSurface(surface);
+    }
+
+    void render(SDL_Renderer *renderer, std::string input, int x, int y){
+        int i = 0;
+        for(auto c : input){
+            if(c >= '0' && c <= '9'){
+                //Get character source from map
+                this->source.x = 8 + 11*(c - '0');
+                this->source.y = 49;
+                this->source.w = 11;
+                this->source.h = 15;
+
+                //Get destination source, using specified x and y coordinates (starting from top left corner)
+                this->dest.x = x + 11*(i++);
+                this->dest.y = y;
+                this->dest.w = 12;
+                this->dest.h = 16;
+
+                SDL_RenderCopy(renderer, this->texture, &this->source, &dest);
+            }
+            else if(c >= 'z' && c <= 'a'){
+                //TODO
+            }
+            else if(c >= 'Z' && c <= 'A'){
+                //TODO
+            }
+        }
+    }
+
+    void clearRender(){
+
+    }
+
+    ~TextRenderer(){
+        SDL_DestroyTexture(texture);
+    }
+};
