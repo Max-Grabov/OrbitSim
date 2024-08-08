3cb4b2fd38a3c7aea996e14324865bc42e41a04d
MaxG <maxpgrabov@gmail.com>
2024-08-07 21:05:36 -0400

    Made Number pasting the same as character pasting

--- src/include/selfIncludes/textRenderer.h
+++ src/include/selfIncludes/textRenderer.h
@@ -19,7 +19,7 @@
 //Lower and upper case x values, There is an extra integer to account for Z in the rendering function without having if checks
 const int lowerLetterX[] = {8, 19, 30, 41, 52, 63, 71, 82, 93, 97, 108, 117, 122, 133, 144, 155, 165, 176, 187, 198, 205, 216, 227, 238, 248, 259, 269};
 const int upperLetterX[] = {8, 19, 30, 41, 52, 63, 74, 84, 95, 102, 113, 124, 135, 145, 156, 167, 178, 189, 200, 210, 221, 232, 242, 253, 264, 275, 286};
-
+const int numX[] = {9, 20, 30, 41, 52, 63, 73, 84, 95, 106, 116};
 class TextRenderer {
 public:
 
@@ -39,16 +39,10 @@ public:
         for(auto c : input){
             if(c >= '0' && c <= '9'){
                 //Get character source from map
-                this->source.x = NUM_STARTX + NUM_WIDTH*(c - '0');
+                this->source.x = numX[c - '0'];
                 this->source.y = NUM_STARTY;
-                this->source.w = NUM_WIDTH;
+                this->source.w = numX[c - 'a' + 1] - numX[c - 'a'];
                 this->source.h = NUM_HEIGHT;
-
-                //Get destination source, using specified x and y coordinates (starting from top left corner)
-                this->dest.x = x + currX;
-                this->dest.y = y;
-                this->dest.w = NUM_WIDTH;
-                this->dest.h = NUM_HEIGHT;
             }
 
             else if(c >= 'a' && c <= 'z'){
@@ -56,11 +50,6 @@ public:
                 this->source.y = LOWER_CASE_STARTY;
                 this->source.w = lowerLetterX[c - 'a' + 1] - lowerLetterX[c - 'a'];
                 this->source.h = LOWER_CASE_HEIGHT;
-
-                this->dest.x = x + currX;
-                this->dest.y = y;
-                this->dest.w = this->source.w;
-                this->dest.h = this->source.h;
             }
 
             else if(c >= 'A' && c <= 'Z'){
@@ -68,11 +57,6 @@ public:
                 this->source.y = UPPER_CASE_STARTY;
                 this->source.w = upperLetterX[c - 'A' + 1] - upperLetterX[c - 'A'];
                 this->source.h = UPPER_CASE_HEIGHT;
-
-                this->dest.x = x + currX;
-                this->dest.y = y;
-                this->dest.w = this->source.w;
-                this->dest.h = this->source.h;
             }
 
             else if(c == ' '){
@@ -81,12 +65,12 @@ public:
                 this->source.y = LOWER_CASE_STARTY;
                 this->source.w = NUM_WIDTH;
                 this->source.h = LOWER_CASE_HEIGHT;
-
-                this->dest.x = x + currX;
-                this->dest.y = y;
-                this->dest.w = this->source.w;
-                this->dest.h = this->source.h;
             }
+            this->dest.x = x + currX;
+            this->dest.y = y;
+            this->dest.w = this->source.w;
+            this->dest.h = this->source.h;
+
             currX += this->source.w;
             SDL_RenderCopy(renderer, this->texture, &this->source, &dest);
         }
